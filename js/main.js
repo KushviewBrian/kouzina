52e33a42291adb03f6928b66af502b6d05486deb
Brian Tennyson <brian@kushview.net>
2025-08-31 23:00:43 -0400

    update

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.nav-toggle');
-const links = document.querySelector('.nav-links');
-toggle?.addEventListener('click', ()=>{
-  const open = links.style.display === 'block';
-  links.style.display = open ? 'none' : 'block';
-});
